40172ab7ea9872492b16e4f7eb8431b8c43c3252
bhagi221206 <bhagi221206@gmail.com>
2026-04-06 20:17:54 +0530

    Add files via upload

---
+++ Multiplication Table - 3.py
@@ -0,0 +1,4 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> n, a, b = map(int, input().split())
+... for i in range(a, b+1):
