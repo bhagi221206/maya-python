b1aa09af1304c57d077efb27e4a1def1ce3dc2c8
bhagi221206 <bhagi221206@gmail.com>
2026-04-06 20:04:08 +0530

    Add files via upload

---
+++ Character is Upper case Or Lower case.py
@@ -0,0 +1,7 @@
+ch = input().strip()
+if ch >= 'A' and ch <= 'Z':
+    print("uppercase alphabet")
+elif ch >= 'a' and ch <= 'z':
+    print("lowercase alphabet")
+else:
+    print("not an alphabet")
